363310256aeaf5c6caab51d665d357b78ca667b5
rlewicki <contact@rlewicki.tech>
2022-11-16 17:55:24 +0100

    Display current humidity and temperature readings from DHT11 sensor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import machine
+import time
+from machine import Pin
 from grove_lcd import Grove_RGB_LCD
+from dht11 import DHT11
 
 lcd = Grove_RGB_LCD(16, 2, 0)
 
@@ -17,3 +19,14 @@ heart.append(0b00000)
 lcd.createChar(0, heart)
 lcd.setCursor(1, 1)
 lcd.write(0)
+
+pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(pin)
+while True:
+    time.sleep(1)
+    sensor = DHT11(pin)
+    t  = (sensor.temperature)
+    h = (sensor.humidity)
+    print("Temperature: {}".format(sensor.temperature))
+    print("Humidity: {}".format(sensor.humidity))
+    
