4fb005aee54fa3ea5175bb5fd7433deb3ca32b31
rlewicki <contact@rlewicki.tech>
2023-01-06 11:36:18 +0100

    Add network connection to a local wifi

--- pico/main.py
+++ pico/main.py
@@ -3,13 +3,34 @@ from machine import Pin
 from grove_lcd import Grove_RGB_LCD
 from dht11 import DHT11
 from base64 import b64encode
+import network
 
 lcd = Grove_RGB_LCD(16, 2, 0)
-
 lcd.home()
 lcd.noBlink()
 lcd.noCursor()
-lcd.print("Initializing...")
+lcd.print("Booting up")
+
+time.sleep(1)
+
+lcd.clear()
+lcd.setCursor(0, 0)
+lcd.print("Network init")
+
+ssid = ""
+password = ""
+
+with open("ssid.txt", "r") as f:
+    ssid = f.read()
+
+with open("password.txt", "r") as f:
+    password = f.read()
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+while not wlan.isconnected():
+    time.sleep(1)
+print(wlan.ifconfig())
 
 degree_char = bytearray(0)
 degree_char.append(0b00010)
@@ -36,3 +57,4 @@ while True:
     lcd.print("C")
     lcd.setCursor(0, 1)
     lcd.print("H: {}%".format(h))
+
