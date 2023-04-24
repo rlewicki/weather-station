73717189f12b66a2ed8d27d2136c137d2f013200
rlewicki <contact@rlewicki.tech>
2023-04-24 14:04:50 +0200

    Running sensor readings now on a separate thread so it's not blocked by the HTTP socket

--- pico/main.py
+++ pico/main.py
@@ -6,7 +6,9 @@ from dht11 import InvalidChecksum
 from dht11 import InvalidPulseCount
 from base64 import b64encode
 import network
+import socket
 import ntptime
+import _thread
 
 def http_get(url):
     import socket
@@ -64,22 +66,73 @@ button_pin.irq(trigger=Pin.IRQ_FALLING, handler=switch_display_callback)
 dht_pin = Pin(28)
 sensor = DHT11(dht_pin)
 sensor_errors_count = 0
-while True:
-    if should_display_time:
-        current_time = time.localtime()
-        print("{}:{}:{}".format(current_time[3] + 1, current_time[4], current_time[5]))
-        time.sleep(1)
-    else:
+
+html = """<!DOCTYPE html>
+<html>
+    <head> <title>Pico W</title> </head>
+    <body> <h1>Pico W</h1>
+    </body>
+</html>
+"""
+
+host_addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
+http_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+http_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+http_socket.bind(host_addr)
+http_socket.listen(1)
+
+def get_current_date():
+    current_time = time.localtime()
+    current_time = "{}:{}:{}".format(current_time[3] + 1, current_time[4], current_time[5])
+    return current_time;
+
+def get_temperature_and_humidity():
+    sensor.measure()
+    return sensor._temperature, sensor._humidity
+
+global inside_temperature
+global inside_humidity
+global current_date
+
+inside_temperature = 0
+inside_humidity = 0
+current_date = ""
+
+def update_data():
+    global inside_temperature
+    global inside_humidity
+    global current_date
+    while True:
+        current_date = get_current_date()
         try:
-            sensor.measure()
-            t = sensor._temperature
-            h = sensor._humidity
-            print("T: {}C".format(t))
-            print("H: {}%".format(h))
-            time.sleep(5)
+            inside_temperature, inside_humidity = get_temperature_and_humidity()
         except InvalidChecksum as e:
-            sensor_errors_count += 1
-            print("DHT11 sensor error: invalid checksum")
+            pass
         except InvalidPulseCount as e:
-            sensor_errors_count += 1
-            print("DHT11 sensor error: invalid pulse count")
+            pass
+        time.sleep(5)
+
+def await_http_connection():
+    try:
+        cl, addr = http_socket.accept()
+        print("client connected from", addr)
+        request = cl.recv(1024)
+        print(request)
+        
+        request = str(request)
+        html = "temperatura: {}C, wilgotnosc: {}%, data: {}".format(
+            inside_temperature, 
+            inside_humidity,
+            current_date)
+        response = html
+        cl.send("HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n")
+        cl.send(response)
+        cl.close()
+    except Exception as e:
+        print(e)
+        cl.close()
+
+_thread.start_new_thread(update_data, ())
+
+while True:
+    await_http_connection()
